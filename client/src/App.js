32229336c9923b44cfedb5360d5a97a6cd12a902
Rajni <rajnisandhu487@gmail.com>
2025-01-29 22:51:16 -0500

    loginRegistrationDesigningAndFooterr

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
+import{BrowserRouter, Routes, Route} from 'react-router-dom'
+import LoginSignup from './LoginSignup';
+import Header from './Header';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
-     EduSphere
+    < div className='App'>
+    <Header/>
+    <LoginSignup/>
+    <Footer/>
     </div>
+ 
   );
 }
 
 export default App;
+
+
+
+ // <div className="App">
+    //  {/* EduSphere */}
+    //  <Signup/>
+    // </div>
+ //  <BrowserRouter>
+  //  <Routes>
+  //   <Route path='/' element={<LoginSignup/>}></Route>
+  //  </Routes>
+  //  </BrowserRouter>
---
+++ client/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      hello
+    </header>
+  )
+}
+
+export default Header
